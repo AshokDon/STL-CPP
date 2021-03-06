dc8c8b1503a91e614372c9c83b1e0600dbe6a5d8
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-03-06 09:24:17 +0530

    Add files via upload

---
+++ STL_PAIR.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+#include<bits/stdc++.h>//to import everything
+int main()
+{
+    pair<int , int>p={1,3};
+    std::cout<<p.first<<" "<<p.second;
+    return 0;
+
+}
+
