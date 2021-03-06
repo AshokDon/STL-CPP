4473fbe719aa53d815e20a08601ea5efc7079a87
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-03-06 09:30:35 +0530

    Add files via upload

---
+++ STL_list.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<list>
+#include<iterator>>
+int main()
+{
+    list<int>lis;
+    lis.push_back(1);//{1}
+    lis.emplace_back(2);//{2}
+    //what if you want push at front
+    lis.push_front(5);//{5,1,2}
+    list.emplace_front();
+}
