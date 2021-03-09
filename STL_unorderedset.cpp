4f00b134249c74da41f902652dd88cce52599f31
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-03-09 09:23:37 +0530

    Add files via upload

---
+++ STL_unorderedset.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+    unordered_set<int>us;//does't store duplicates and they are
+    //in unsorted
+    //it does not store in any particular order
+    //it has a better time complexity
+    //but in case of collusions gives wrong answer
+
+}
