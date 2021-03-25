e9fb0cce2960677d88a0596cce194209d83967b0
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-03-25 19:14:30 +0530

    Update and rename STL_multiset.cpp to 7th_STL_multiset.cpp

--- 7th_STL_multiset.cpp
+++ 7th_STL_multiset.cpp
@@ -12,7 +12,7 @@ int main()
     for(auto it:ms){
         cout<<it<<" ";
     }
-    ms.count(1);//give the count ones in set
+    ms.count(1);//give the count of ones in set
     //what if you want erase two ones
     ms.erase(ms.find(1),ms.find(1)+2);
 }
